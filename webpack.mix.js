d356ebdcdb1f82b21367f01c32a093c78bf341b0
Parthib Dutta <duttaparthib@yahoo.in>
2020-03-20 02:22:10 +0530

    updated mix config for better debugging

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+let productionSourceMaps = false;
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').sourceMaps(productionSourceMaps, 'source-map')
     .sass('resources/sass/app.scss', 'public/css');
